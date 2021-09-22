8435d744c557fb63f11389e233b642c1153209f2
nicolanzi1 <nicolanzi@gmail.com>
2021-09-22 15:42:46 +0100

    Built a Huddle app with React, styled components and react icons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from 'styled-components'
+import Header from './components/Header'
+import Card from './components/Card'
+import Footer from './components/Footer'
+import { Container } from './components/styles/Container.styled'
+import GlobalStyles from './components/Global'
+import content from './content'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const theme = {
+	colors: {
+		header: '#ebfbff',
+		body: '#fff',
+		footer: '#003333',
+	},
+	mobile: '768px',
 }
 
-export default App;
+function App() {
+	return (
+		<ThemeProvider theme={theme}>
+			<>
+				<GlobalStyles />
+				<Header />
+				<Container>
+					{content.map((item, index) => (
+						<Card key={index} item={item} />
+					))}
+				</Container>
+				<Footer />
+			</>
+		</ThemeProvider>
+	)
+}
+
+export default App
